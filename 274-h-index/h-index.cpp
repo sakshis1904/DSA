1dde7a5b97672ca9133a1e400417e8292855afa4
sakshis1904 <150705225+sakshis1904@users.noreply.github.com>
2025-06-25 14:19:02 +0530

    Time: 1 ms (20.51%) | Memory: 13.8 MB (6.29%) - LeetSync

---
+++ 274-h-index/h-index.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+       sort(citations.rbegin(), citations.rend()); // Sort in descending order
+        int h = 0;
+        for (int i = 0; i < citations.size(); i++) {
+            if (citations[i] >= i + 1) {
+                h = i + 1;
+            } else {
+                break;
+            }
+        }
+        return h; 
+    }
+};
