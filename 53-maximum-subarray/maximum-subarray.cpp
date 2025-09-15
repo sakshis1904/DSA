ea2691f1f43f2e8604402b90618eb757cdbbce94
sakshis1904 <150705225+sakshis1904@users.noreply.github.com>
2025-09-15 11:09:37 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (52.04%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -6,14 +6,14 @@ public:
 
         for(int i=0; i<nums.size(); i++){
             sum += nums[i];
+
             if(sum > maxi){
-               maxi = sum;
+                maxi = sum;
             } 
             if(sum < 0){
-               sum = 0;
+                sum = 0;
             }
         }
         return maxi;
-
     }
 };
