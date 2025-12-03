8b71ff9d344c651abff42a437031804e530fb2e9
sakshis1904 <150705225+sakshis1904@users.noreply.github.com>
2025-12-03 22:21:47 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (85.89%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        if (grid.empty() || grid[0].empty()) return 0;
+        int m = grid.size(), n = grid[0].size();
+
+        // first row: accumulate from left
+        for (int j = 1; j < n; ++j)
+            grid[0][j] += grid[0][j-1];
+
+        // first column: accumulate from top
+        for (int i = 1; i < m; ++i)
+            grid[i][0] += grid[i-1][0];
+
+        // rest: choose min from top or left
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                grid[i][j] += min(grid[i-1][j], grid[i][j-1]);
+            }
+        }
+
+        return grid[m-1][n-1];
+    }
+};
