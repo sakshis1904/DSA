29a3755a772b1c332ec81445ae44a2014ea51da5
sakshis1904 <150705225+sakshis1904@users.noreply.github.com>
2025-12-27 23:01:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (42.93%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long p = n;
+        if (p < 0) {
+            x = 1 / x;
+            p = -p;
+        }
+        double res = 1.0;
+        while (p) {
+            if (p & 1) res *= x;
+            x *= x;
+            p >>= 1;
+        }
+        return res;
+    }
+};
