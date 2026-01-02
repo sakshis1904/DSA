8c1228bf18afa6fab865b5ab58ed28ebbcf529e2
sakshis1904 <150705225+sakshis1904@users.noreply.github.com>
2026-01-02 18:10:12 +0530

    Time: 2 ms (37.09%) | Memory: 13.2 MB (86.04%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+
+        for (int bit = 0; bit < 32; bit++) {
+            int count = 0;
+            for (int num : nums) {
+                if ((num >> bit) & 1) {
+                    count++;
+                }
+            }
+            if (count % 3 == 1) {
+                result |= (1 << bit);
+            }
+        }
+        return result;
+    }
+};
